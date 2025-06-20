81d6b3adc7d0db3af9d14e0862866fcfd3e92c62
Samuel <samuelstolle@posteo.de>
2025-06-20 09:09:16 +0200

    Rename imported main file to avoid confusions

---
+++ heka_reader_hallermanlab/plot_traces.py
@@ -0,0 +1,17 @@
+import heka_reader
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+IMPORT_FOLDER = "/Users/stefanhallermann/Library/CloudStorage/Dropbox/tmp/Sophie/old/in"
+filename = "Sophie_2025-04-17_002.dat"
+heka_path = os.path.join(IMPORT_FOLDER, filename)
+print("Reading from " + heka_path)
+bundle = heka_reader.Bundle(heka_path)
+trace = bundle.data[0, 4, 0, 1]
+node = bundle.pul[0][4][0][1]
+time = np.linspace(node.XStart, node.XStart + node.XInterval * (len(trace) - 1), len(trace))
+plt.plot(time * 1000, trace)  # time in ms
+plt.xlabel("Time (ms)")
+plt.ylabel(f"{node.Label} ({node.YUnit})")
+plt.show()
